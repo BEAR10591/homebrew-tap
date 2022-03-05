481c9f0dcae8a689f14dbba7e609b38cef6bfd8f
BEAR10591 <99163761+BEAR10591@users.noreply.github.com>
2022-03-05 16:19:42 +0900

    This installation requires --HEAD

--- libaribcaption.rb
+++ libaribcaption.rb
@@ -1,13 +1,13 @@
 class Libaribcaption < Formula
-    desc "Portable ARIB STD-B24 Caption Decoder/Renderer"
-    homepage "https://github.com/xqq/libaribcaption"
-    head "https://github.com/xqq/libaribcaption.git", branch: "master"
+     desc "Portable ARIB STD-B24 Caption Decoder/Renderer"
+     homepage "https://github.com/xqq/libaribcaption"
+     head "https://github.com/xqq/libaribcaption.git", branch: "master"
 
-    depends_on "cmake" => :build
+     depends_on "cmake" => :build
 
-    def install
-        system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}"
-        system "cmake", "--build", "-j8"
-        system "cmake", "--install"
-    end
-  end
+     def install
+         system "cmake", ".", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+         system "cmake", "--build", ".", "-j8"
+         system "cmake", "--install", "."
+     end
+   end
