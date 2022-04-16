97b3eb9c77185c923910ba56355b2f04a8f26674
BEAR10591 <99163761+BEAR10591@users.noreply.github.com>
2022-04-17 03:19:19 +0900

    mpv.app

--- mpv.rb
+++ mpv.rb
@@ -4,31 +4,61 @@ class Mpv < Formula
   url "https://github.com/mpv-player/mpv/archive/v0.34.1.tar.gz"
   sha256 "32ded8c13b6398310fa27767378193dc1db6d78b006b70dbcbd3123a1445e746"
   license :cannot_represent
-  revision 1
   head "https://github.com/mpv-player/mpv.git", branch: "master"
 
   depends_on "docutils" => :build
   depends_on "pkg-config" => :build
-  depends_on "python@3.9" => :build
+  depends_on "python@3.10" => :build
 
   depends_on "bear10591/tap/ffmpeg"
+  depends_on "yt-dlp/taps/yt-dlp"
+  depends_on "libass"
   depends_on "jpeg"
   depends_on "libarchive"
-  depends_on "libass"
   depends_on "little-cms2"
   depends_on "luajit-openresty"
   depends_on "mujs"
+  depends_on "rubberband"
   depends_on "uchardet"
   depends_on "vapoursynth"
-  depends_on "yt-dlp/taps/yt-dlp"
+  depends_on "zimg"
 
-  fails_with gcc: "5" # ffmpeg is compiled with GCC
+  depends_on "subliminal" => :recommended
+
+  depends_on "jack" => :optional
+  depends_on "libaacs" => :optional
+  depends_on "libbluray" => :optional
+  depends_on "libcaca" => :optional
+  depends_on "libcdio" => :optional
+  depends_on "libdvdnav" => :optional
+  depends_on "libdvdread" => :optional
+  depends_on "sdl2" => :optional
+
+  on_macos do
+    depends_on "bear10591/tap/dockutil@2" => :recommended if MacOS.version <  :big_sur
+    depends_on "bear10591/tap/dockutil@3" => :recommended if MacOS.version >= :big_sur
+    depends_on "coreutils" => :recommended
+    depends_on "tag" => :recommended
+    depends_on "trash" => :recommended
+  end
 
   def install
+    opts  = Hardware::CPU.arm? ? "-mcpu=native " : "-march=native -mtune=native "
+    opts += "-Ofast -flto=thin -funroll-loops -fomit-frame-pointer "
+    opts += "-ffunction-sections -fdata-sections -fstrict-vtable-pointers -fwhole-program-vtables "
+    opts += "-fforce-emit-vtables " if MacOS.version >= :mojave
+    ENV.append "CFLAGS",      opts
+    ENV.append "CPPFLAGS",    opts
+    ENV.append "CXXFLAGS",    opts
+    ENV.append "OBJCFLAGS",   opts
+    ENV.append "OBJCXXFLAGS", opts
+    ENV.append "LDFLAGS",     opts + " -dead_strip"
+
     # LANG is unset by default on macOS and causes issues when calling getlocale
     # or getdefaultlocale in docutils. Force the default c/posix locale since
     # that's good enough for building the manpage.
-    ENV["LC_ALL"] = "C"
+    ENV["LC_ALL"] = "en_US.UTF-8"
+    ENV["LANG"]   = "en_US.UTF-8"
 
     # libarchive is keg-only
     ENV.prepend_path "PKG_CONFIG_PATH", Formula["libarchive"].opt_lib/"pkgconfig"
@@ -37,23 +67,22 @@ class Mpv < Formula
 
     args = %W[
       --prefix=#{prefix}
-      --enable-html-build
-      --enable-javascript
-      --enable-libmpv-shared
-      --enable-lua
-      --enable-libarchive
-      --enable-uchardet
       --confdir=#{etc}/mpv
       --datadir=#{pkgshare}
-      --mandir=#{man}
       --docdir=#{doc}
+      --mandir=#{man}
       --zshdir=#{zsh_completion}
-      --lua=luajit
-    ]
 
-    system Formula["python@3.9"].opt_bin/"python3", "bootstrap.py"
-    system Formula["python@3.9"].opt_bin/"python3", "waf", "configure", *args
-    system Formula["python@3.9"].opt_bin/"python3", "waf", "install"
+      --disable-html-build
+      --enable-libmpv-shared
+    ]
+    args << "--swift-flags=-O -wmo"
+
+    system Formula["python@3.10"].opt_bin/"python3", "bootstrap.py"
+    system Formula["python@3.10"].opt_bin/"python3", "waf", "configure", *args
+    system Formula["python@3.10"].opt_bin/"python3", "waf", "install"
+    system Formula["python@3.10"].opt_bin/"python3", "TOOLS/osxbundle.py", "build/mpv"
+    prefix.install "build/mpv.app"
   end
 
   test do
