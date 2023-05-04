867fd29e5504d1121abdce0c6972881819679014
BEAR10591 <bear10591@icloud.com>
2023-05-05 00:08:29 +0900

    Update mpv.rb

--- mpv.rb
+++ mpv.rb
@@ -4,6 +4,7 @@ class Mpv < Formula
   url "https://github.com/mpv-player/mpv/archive/refs/tags/v0.35.1.tar.gz"
   sha256 "41df981b7b84e33a2ef4478aaf81d6f4f5c8b9cd2c0d337ac142fc20b387d1a9"
   license :cannot_represent
+  revision 2
   head "https://github.com/mpv-player/mpv.git", branch: "master"
 
   head do
@@ -14,9 +15,9 @@ class Mpv < Formula
 
   depends_on "docutils" => :build
   depends_on "meson" => :build
-  depends_on "pkg-config" => :build
+  depends_on "pkg-config" => [:build, :test]
   depends_on xcode: :build
-  depends_on "bear10591/tap/ffmpeg"
+  depends_on "ber10591/tap/ffmpeg"
   depends_on "jpeg-turbo"
   depends_on "libarchive"
   depends_on "libass"
@@ -44,7 +45,6 @@ class Mpv < Formula
     ENV.prepend_path "PKG_CONFIG_PATH", Formula["libarchive"].opt_lib/"pkgconfig"
 
     args = %W[
-      -Ddvbin=enabled
       -Dhtml-build=enabled
       -Djavascript=enabled
       -Dlibmpv=true
@@ -60,12 +60,29 @@ class Mpv < Formula
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
 
-    inreplace "TOOLS/dylib-unhell.py", "libraries(lib, result)", "lib = lib.replace(\"@loader_path\", \"" + "#{HOMEBREW_PREFIX}/lib" + "\"); libraries(lib, result)"
+    if OS.mac?
+      # `pkg-config --libs mpv` includes libarchive, but that package is
+      # keg-only so it needs to look for the pkgconfig file in libarchive's opt
+      # path.
+      libarchive = Formula["libarchive"].opt_prefix
+      inreplace lib/"pkgconfig/mpv.pc" do |s|
+        s.gsub!(/^Requires\.private:(.*)\blibarchive\b(.*?)(,.*)?$/,
+                "Requires.private:\\1#{libarchive}/lib/pkgconfig/libarchive.pc\\3")
+      end
+    end
+
+    bash_completion.install "etc/mpv.bash-completion" => "mpv"
+    zsh_completion.install "etc/_mpv.zsh" => "_mpv"
+
+    inreplace "TOOLS/dylib-unhell.py", "libraries(lib, result)",
+              "lib = lib.replace(\"@loader_path\", \"" + "#{HOMEBREW_PREFIX}/lib" + "\"); libraries(lib, result)"
+    inreplace "TOOLS/dylib-unhell.py", "libraries(lib, result)",
+              "lib = lib.replace(      \"@rpath\", \"" + "#{HOMEBREW_PREFIX}/lib" + "\"); libraries(lib, result)"
     system "python3.11", "TOOLS/osxbundle.py", "build/mpv"
     bindir = "build/mpv.app/Contents/MacOS/"
     rm   bindir + "mpv-bundle"
-    mv   bindir + "mpv",        bindir + "mpv-bundle"
-    ln_s bindir + "mpv-bundle", bindir + "mpv"
+    mv   bindir + "mpv", bindir + "mpv-bundle"
+    ln_s "mpv-bundle", bindir + "mpv"
     system "codesign", "--deep", "-fs", "-", "build/mpv.app"
     prefix.install "build/mpv.app"
   end
@@ -73,5 +90,8 @@ class Mpv < Formula
   test do
     system bin/"mpv", "--ao=null", "--vo=null", test_fixtures("test.wav")
     assert_match "vapoursynth", shell_output(bin/"mpv --vf=help")
+
+    # Make sure `pkg-config` can parse `mpv.pc` after the `inreplace`.
+    system "pkg-config", "mpv"
   end
 end
