fbf734c41588c97c58b9f51db3f20a24b80d7889
BEAR10591 <bear10591@icloud.com>
2023-05-04 23:31:54 +0900

    Update mpv.rb and mpv-iina.rb

--- mpv-iina.rb
+++ mpv-iina.rb
@@ -6,12 +6,9 @@ class MpvIina < Formula
   license :cannot_represent
   head "https://github.com/mpv-player/mpv.git", branch: "master"
 
-  stable do
-    def patches
-      [
-        "https://github.com/mpv-player/mpv/commit/0da0acdae8e729eecfb2498ac11cb86a7fe3360d.patch",
-        "https://github.com/mpv-player/mpv/pull/11648.patch"
-      ]
+  head do
+    patch do
+      url "https://raw.githubusercontent.com/BEAR10591/homebrew-tap/main/patch/mpv_PR_11648.patch"
     end
   end
 
