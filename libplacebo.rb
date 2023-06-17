5c0fb60b7a2d0609a4e50ce54136c934470d8106
BEAR10591 <bear10591@icloud.com>
2023-06-18 00:35:14 +0900

    Update libplacebo.rb.

--- libplacebo.rb
+++ libplacebo.rb
@@ -27,16 +27,6 @@ class Libplacebo < Formula
     end
   end
 
-  bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "9d6423a4c9ff3d326b068f0f10abc389769701e617b15dbd0d38871da16a6155"
-    sha256 cellar: :any,                 arm64_monterey: "39ff9ddf8b2ba333da3bc325645ae99dde829caa0703ed230950d9171eca32d2"
-    sha256 cellar: :any,                 arm64_big_sur:  "2b234b5b0948585ce886cb70fb8c03f6f4539ac72d65cd96323b54643b3890ba"
-    sha256 cellar: :any,                 ventura:        "55383e879f4d04e5a3b742a48516e11483b301becbd7c0f54d4c08a378e887c3"
-    sha256 cellar: :any,                 monterey:       "c7fa2501c6c647d138c1ef8b3a4ca376240e914634718f681f94e7190321f983"
-    sha256 cellar: :any,                 big_sur:        "5ec8ab7ada6f94318d2f036bbfe30ac6323d23957353f1a586ef62d2db6e0847"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "724174e2c690bdad334c6bdaa48e2163c5a44a352d10d980d47f2c6d2879f826"
-  end
-
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "python@3.11" => :build
