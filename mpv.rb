dc2dcc2ca645865914063c20255932920f4af499
BEAR10591 <bear10591@icloud.com>
2023-11-18 16:45:20 +0900

    Update mpv.rb.

--- mpv.rb
+++ mpv.rb
@@ -72,10 +72,8 @@ class Mpv < Formula
       end
     end
 
-    if MacOS.version >= :big_sur
-      bash_completion.install "etc/mpv.bash-completion" => "mpv"
-      zsh_completion.install "etc/_mpv.zsh" => "_mpv"
-    end
+    bash_completion.install "etc/mpv.bash-completion" => "mpv"
+    zsh_completion.install "etc/_mpv.zsh" => "_mpv"
 
     # Build, Fix, and Codesign App Bundle
     system "python3.12", "TOOLS/osxbundle.py", "build/mpv", "--skip-deps"
