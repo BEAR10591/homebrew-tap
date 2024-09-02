a34bfbf1cbe9a8499264dbd38dbb78761b0fb053
BEAR10591 <bear10591@icloud.com>
2024-09-02 13:13:02 +0900

    Update repository.

--- mkvtoolnix.rb
+++ mkvtoolnix.rb
@@ -1,10 +1,20 @@
 class Mkvtoolnix < Formula
   desc "Matroska media files manipulation tools"
   homepage "https://mkvtoolnix.download/"
-  url "https://mkvtoolnix.download/sources/mkvtoolnix-83.0.tar.xz"
-  mirror "https://fossies.org/linux/misc/mkvtoolnix-83.0.tar.xz"
-  sha256 "6a8615436406c7fa45bfb2b6270da1bf06ea54cfcd13c3699643833e1d73ecbc"
   license "GPL-2.0-or-later"
+  revision 2
+
+  stable do
+    url "https://mkvtoolnix.download/sources/mkvtoolnix-86.0.tar.xz"
+    mirror "https://fossies.org/linux/misc/mkvtoolnix-86.0.tar.xz"
+    sha256 "29a9155fbba99f9074de2abcfbdc4e966ea38c16d9f6f547cf2d8d9a48152c97"
+
+    # Compatibility with fmt 11. Remove in next release.
+    patch do
+      url "https://gitlab.com/mbunkus/mkvtoolnix/-/commit/b57dde69dc80b151844e0762a2ae6bca3ba86d95.diff"
+      sha256 "602e0d5fce2ef082f4aecc715352cecb632f99493b8132575ad4c8fc9239579b"
+    end
+  end
 
   livecheck do
     url "https://mkvtoolnix.download/sources/"
@@ -39,6 +49,7 @@ class Mkvtoolnix < Formula
 
   uses_from_macos "libxslt" => :build
   uses_from_macos "ruby" => :build
+  uses_from_macos "zlib"
 
   fails_with gcc: "5"
 
@@ -76,8 +87,8 @@ class Mkvtoolnix < Formula
       Homebrew
     EOS
 
-    system "#{bin}/mkvmerge", "-o", mkv_path, sub_path
-    system "#{bin}/mkvinfo", mkv_path
-    system "#{bin}/mkvextract", "tracks", mkv_path, "0:#{sub_path}"
+    system bin/"mkvmerge", "-o", mkv_path, sub_path
+    system bin/"mkvinfo", mkv_path
+    system bin/"mkvextract", "tracks", mkv_path, "0:#{sub_path}"
   end
 end
