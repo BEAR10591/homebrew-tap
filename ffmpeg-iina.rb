c9bbb15a580885bc9b059ca08e62d8d5ce697fe9
BEAR10591 <bear10591@icloud.com>
2024-09-04 01:49:22 +0900

    Update repository.

---
+++ ffmpeg-iina.rb
@@ -0,0 +1,102 @@
+class Ffmpeg < Formula
+  desc "Play, record, convert, and stream audio and video"
+  homepage "https://ffmpeg.org/"
+  url "https://ffmpeg.org/releases/ffmpeg-7.0.2.tar.xz"
+  sha256 "8646515b638a3ad303e23af6a3587734447cb8fc0a0c064ecdb8e95c4fd8b389"
+  # None of these parts are used by default, you have to explicitly pass `--enable-gpl`
+  # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
+  license "GPL-2.0-or-later"
+  head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
+
+  livecheck do
+    url "https://ffmpeg.org/download.html"
+    regex(/href=.*?ffmpeg[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  keg_only <<EOS
+it is intended to only be used for building IINA.
+This formula is not recommended for daily use and has no binaraies (ffmpeg, ffplay etc.)
+EOS
+
+  depends_on "pkg-config" => :build
+  depends_on "dav1d"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "frei0r"
+  depends_on "gnutls"
+  depends_on "libaribcaption"
+  depends_on "libass"
+  depends_on "libbluray"
+  depends_on "libsoxr"
+  depends_on "libvidstab"
+  depends_on "rubberband"
+  depends_on "snappy"
+  depends_on "speex"
+  # depends_on "tesseract"
+  depends_on "xz"
+  depends_on "zeromq"
+  depends_on "zimg"
+  depends_on "jpeg-xl" # for JPEG-XL format screenshot
+  depends_on "webp" # for webp format screenshot
+
+  on_intel do
+    depends_on "nasm" => :build
+  end
+
+  uses_from_macos "bzip2"
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  def install
+
+    args = %W[
+      --prefix=#{prefix}
+      --enable-shared
+      --enable-pthreads
+      --enable-version3
+      --host-cflags=#{ENV.cflags}
+      --host-ldflags=#{ENV.ldflags}
+      --enable-ffplay
+      --enable-gnutls
+      --enable-gpl
+      --enable-libaribcaption
+      --enable-libbluray
+      --enable-libdav1d
+      --enable-librubberband
+      --enable-libsnappy
+      --disable-libtesseract
+      --enable-libvidstab
+      --enable-libxml2
+      --enable-libfontconfig
+      --enable-libfreetype
+      --enable-frei0r
+      --enable-libass
+      --enable-libspeex
+      --enable-libsoxr
+      --enable-videotoolbox
+      --enable-audiotoolbox
+      --enable-libzmq
+      --enable-libzimg
+      --enable-libwebp
+      --enable-libjxl
+      --disable-libjack
+      --disable-indev=jack
+      --disable-programs
+    ]
+
+
+    args << "--enable-neon" if Hardware::CPU.arm?
+    args << "--cc=#{ENV.cc}" if Hardware::CPU.intel?
+
+    system "./configure", *args
+    system "make", "install"
+
+  end
+
+  test do
+    # Create an example mp4 file
+    mp4out = testpath/"video.mp4"
+    system bin/"ffmpeg", "-filter_complex", "testsrc=rate=1:duration=1", mp4out
+    assert_predicate mp4out, :exist?
+  end
+end
