cce0e2a98dda9740c84e282d231a93ddde6c5cbf
BEAR10591 <bear10591@icloud.com>
2024-11-22 23:25:46 +0900

    Update repository.

--- mpv-iina.rb
+++ mpv-iina.rb
@@ -1,36 +1,39 @@
 class MpvIina < Formula
   desc "Media player based on MPlayer and mplayer2"
   homepage "https://mpv.io"
-  head "https://github.com/iina/mpv.git", :branch => "iina-release/1.4.0"
+  url "https://github.com/mpv-player/mpv/archive/refs/tags/v0.39.0.tar.gz"
+  sha256 "2ca92437affb62c2b559b4419ea4785c70d023590500e8a52e95ea3ab4554683"
+  license :cannot_represent
+  head "https://github.com/mpv-player/mpv.git", branch: "master"
 
   keg_only <<EOS
 it is intended to only be used for building IINA. This formula is not recommended for daily use
 EOS
 
-  head do
+  stable do
     patch do # sub/sd_lavc: check decoder output type for dvb and arib
-      url "https://patch-diff.githubusercontent.com/raw/mpv-player/mpv/pull/13752.patch"
-      sha256 "6a62ecccf1862526d0c76abbb53916855eb2915871bff0bcf7d72ad11bda0b80"
+      url "https://github.com/mpv-player/mpv/compare/master...iina:mpv:iina-release/1.4.0.patch"
+      sha256 "7d06eecd564d5a32cbd190cf30e1122d8bb9a932ae98ff7c76a382b7589f0fd6"
     end
   end
 
   depends_on "docutils" => :build
   depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => [:build, :test]
   depends_on xcode: :build
-
   depends_on "bear10591/tap/ffmpeg-iina"
   depends_on "jpeg-turbo"
   depends_on "libarchive"
   depends_on "libass"
+  depends_on "libbluray"
   depends_on "libplacebo"
   depends_on "little-cms2"
   depends_on "luajit"
-  depends_on "libbluray"
-  depends_on "libsamplerate"
+  depends_on "rubberband"
   depends_on "vulkan-loader"
   depends_on "zimg"
- # depends_on "molten-vk"
+  # depends_on "molten-vk"
 
   uses_from_macos "zlib"
 
@@ -46,7 +49,7 @@ EOS
     ENV["LC_ALL"] = "C"
 
     # force meson find ninja from homebrew
-    ENV["NINJA"] = Formula["ninja"].opt_bin/"ninja"
+    ENV["NINJA"] = which("ninja")
 
     # libarchive is keg-only
     ENV.prepend_path "PKG_CONFIG_PATH", Formula["libarchive"].opt_lib/"pkgconfig"
