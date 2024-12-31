7ed25a5e54be7e5dde38ef0f722f173a03cf78c7
BEAR10591 <bear10591@icloud.com>
2024-12-31 14:11:30 +0900

    Update repository.

--- mkvtoolnix.rb
+++ mkvtoolnix.rb
@@ -1,9 +1,9 @@
 class Mkvtoolnix < Formula
   desc "Matroska media files manipulation tools"
   homepage "https://mkvtoolnix.download/"
-  url "https://mkvtoolnix.download/sources/mkvtoolnix-88.0.tar.xz"
-  mirror "https://fossies.org/linux/misc/mkvtoolnix-88.0.tar.xz"
-  sha256 "f2f08c0100740668ef8aba7953fe4aed8c04ee6a5b51717816a4b3d529df0a25"
+  url "https://mkvtoolnix.download/sources/mkvtoolnix-89.0.tar.xz"
+  mirror "https://fossies.org/linux/misc/mkvtoolnix-89.0.tar.xz"
+  sha256 "6127b694b088e53270a946c178b6816da756200bdf672abdf65cfe0b50b260b2"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -19,12 +19,14 @@ class Mkvtoolnix < Formula
   end
 
   depends_on "docbook-xsl" => :build
+  depends_on "gettext" => :build
+  depends_on "nlohmann-json" => :build
   depends_on "pkgconf" => :build
+  depends_on "utf8cpp" => :build
   depends_on "boost"
   depends_on "cmark"
   depends_on "flac"
   depends_on "fmt"
-  depends_on "gettext"
   depends_on "gmp"
   depends_on "libebml"
   depends_on "libmatroska"
@@ -32,17 +34,23 @@ class Mkvtoolnix < Formula
   depends_on "libvorbis"
   # https://mkvtoolnix.download/downloads.html#macosx
   depends_on macos: :catalina # C++17
-  depends_on "nlohmann-json"
   depends_on "pugixml"
   depends_on "qt"
-  depends_on "utf8cpp"
 
   uses_from_macos "libxslt" => :build
   uses_from_macos "ruby" => :build
   uses_from_macos "zlib"
 
+  on_macos do
+    depends_on "gettext"
+  end
+
   def install
-    ENV.cxx11
+    # Remove bundled libraries
+    rm_r(buildpath.glob("lib/*") - buildpath.glob("lib/{avilib,librmff}*"))
+
+    # Boost Math needs at least C++14, Qt needs at least C++17
+    ENV.append "CXXFLAGS", "-std=c++17"
 
     features = %w[flac gmp libebml libmatroska libogg libvorbis]
     extra_includes = ""
