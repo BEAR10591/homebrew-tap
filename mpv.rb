fe4e0aab4dce5a882c52097cc92c07d241cd15a4
BEAR10591 <bear10591@icloud.com>
2025-02-02 17:50:32 +0900

    Update repository.

--- mpv.rb
+++ mpv.rb
@@ -4,7 +4,7 @@ class Mpv < Formula
   url "https://github.com/mpv-player/mpv/archive/refs/tags/v0.39.0.tar.gz"
   sha256 "2ca92437affb62c2b559b4419ea4785c70d023590500e8a52e95ea3ab4554683"
   license :cannot_represent
-  revision 2
+  revision 3
   head "https://github.com/mpv-player/mpv.git", branch: "master"
 
   patch do
@@ -93,6 +93,7 @@ class Mpv < Formula
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
     system "meson", "compile", "-C", "build", "macos-bundle"
+    system "tar", "-czvf", "mpv.tar.gz", "-C", "build", "mpv.app"
 
     if OS.mac?
       # `pkg-config --libs mpv` includes libarchive, but that package is
