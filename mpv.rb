6a89b69d4265c58cc6803867c22e15f01b4081bb
BEAR10591 <bear10591@icloud.com>
2025-02-23 23:03:18 +0900

    Update repository.

--- mpv.rb
+++ mpv.rb
@@ -12,6 +12,11 @@ class Mpv < Formula
     sha256 "5b8907575b0f377ef5e621530bc27f56b70e1ba8460f800dc22e349acc788605"
   end
 
+  patch do
+    url "https://raw.githubusercontent.com/BEAR10591/homebrew-tap/refs/heads/main/patch/mpv-console.patch"
+    sha256sum "10d630cbebec16a0093ef1a898d23f4e554f183eabf1a51ca8751a49a7b15e36"
+  end
+
   depends_on "docutils" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
