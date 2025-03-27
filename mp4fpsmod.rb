88c5b1e92b10863fe8ca1cb9e0f78c2c4e728b55
BEAR10591 <bear10591@icloud.com>
2025-03-27 22:22:46 +0900

    Update repository.

---
+++ mp4fpsmod.rb
@@ -0,0 +1,23 @@
+class Mp4fpsmod < Formula
+  desc "mp4 time code editor"
+  homepage "sites.google.com/site/qaacpage/"
+  lisence "Public Domain"
+  head "https://github.com/nu774/mp4fpsmod.git", branch: "master"
+
+  stable do
+	url "https://github.com/nu774/mp4fpsmod/archive/refs/tags/v0.28.tar.gz"
+	sha256 "334fbaa523a7ca16311d7edb25b244b016ec4af6cf438860b687feadaa23b2f8"
+  end
+
+  depends_on "libtool" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+
+  def install
+	system "./bootstrap.sh"
+	system "./configure"
+	system "make"
+	system "strip", "mp4fpsmod"
+	system "make", "install"
+  end
+end
